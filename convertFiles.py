d65f0bf0f6ba83cb912daa21986e31bad4badbc4
zayamatias <32246591+zayamatias@users.noreply.github.com>
2018-08-23 15:31:33 +0200

    Convert from image to sprites done
    To be throughly tested of course

--- convertFiles.py
+++ convertFiles.py
@@ -15,7 +15,7 @@ for colidx in range (0,16):
 def main():
     args=sys.argv
     if len(args)<=2 :
-        print ("Missing arguments, please use:\nconvertFiles.py <inputfile> <outputfile> <image_width (default 1024) and divisible by 16px>\n")
+        print ("Missing arguments, please use:\nconvertFiles.py <inputfile> <outputfile> <image_width (default 1024) and divisible by 16px> \n")
         #sys.exit(1)
     inFile = args[1]
     outFile = args[2]
@@ -26,6 +26,7 @@ def main():
     #Detect what kind of file we're dealing with
     filetypes = [["FIXED","[a-zA-Z]*[_\-][sS][0-9].*"],["SPRITES","[a-zA-Z]*[_\-][cC][0-9].*"]]
     convert = ""
+    # Detect Infile Type
     for filetype in filetypes:
         m = re.search(filetype[1], inFile)
         if m is not None:
@@ -38,12 +39,171 @@ def main():
         convertSprites (inFile,outFile,fileWidth)
         print ("Conversion done")
         sys.exit(0)
-    else:
-        print ("Cannot seem to find the type of ROM you wish to use")
-        sys.exit(1)
+    # Detect Infile Type
+    for filetype in filetypes:
+        m = re.search(filetype[1], outFile)
+        if m is not None:
+            convert = filetype [0]
+    if convert == "FIXED":
+        convertToFixed (inFile,outFile)
+        print ("Conversion done")
+        sys.exit(0)
+    if convert == "SPRITES":
+        convertToSprites (inFile,outFile)
+        print ("Conversion done")
+        sys.exit(0)
 
+    print ("Cannot seem to find the type of ROM you wish to use")
+    sys.exit(1)
+
+def convertToSprites(inFile,outFile):
+    print ("Convert To Sprites")
+    pe = re.compile ("[0-9]")
+    m = pe.search(inFile)
+    searchFile = list(inFile)
+    searchFile[m.span()[1]-1]="?"
+    searchFile="".join(searchFile)
+    foundFiles = glob.glob(searchFile)
+    numFiles = int(len(foundFiles))
+    cFileCount = 1
+    pe = re.compile ("[_-][cC]")
+    m = pe.search(outFile)
+    for fileNum in range (numFiles):
+        oddFileName = list(outFile)
+        oddFileName[m.span()[1]]=str(cFileCount)
+        oddFileName = "".join(oddFileName)
+        cFileCount = cFileCount + 1
+        evenFileName = list(outFile)
+        evenFileName[m.span()[1]]=str(cFileCount)
+        evenFileName = "".join(evenFileName)
+        cFileCount = cFileCount + 1
+        oddFile = open(oddFileName,'wb')
+        evenFile = open(evenFileName,'wb')
+        print ("reading "+foundFiles[fileNum]+" file")
+        #I'm assuming correct order ... to be reviewed!! (TODO)
+        img = PIL.Image.open(foundFiles[fileNum])
+        img = img.convert('RGB')
+        #Check if image has a MAX of 16 colours
+        RGBColors = img.getcolors()
+        if (len(RGBColors))>16:
+            print ("Image has more than 16 colors, it cannot be converted\Remember that this conversion will use the palette index as index for the color in the resulting file.")
+        colors = [0 for i in range (0,16)]
+        idx=15
+        #For some reason the colors are inverted in the palette....This need to be further checked: TODO
+        for RGBColor in RGBColors:
+            colors[idx]=RGBColor[1]
+            idx = idx -1
+        xsize = img.size[0]
+        ysize = img.size[1]
+        tileXsize = 16
+        tilesPerRow = int(xsize / tileXsize)
+        tileYsize = 16
+        tilesPerCol = int(ysize/ tileYsize)
+        tilesNum = int(tilesPerRow*tilesPerCol)
+        for tile in range (tilesNum):
+            oddBytes = bytearray(64)
+            evenBytes = bytearray(64)
+            sprite = []
+            for row in range(tileYsize):
+                sprRow = [0 for i in range (tileXsize)]
+                for col in range(tileXsize):
+                    colOff = int(tile%tilesPerRow)
+                    rowOff = int(tile/tilesPerRow)
+                    pxOff = colOff*tileXsize
+                    pyOff = rowOff*tileYsize
+                    px = pxOff+col
+                    py = pyOff+row
+                    pixel = img.getpixel((px,py))
+                    colIdx = colors.index (pixel)
+                    sprRow[col] = colIdx
+                sprite.append(sprRow)
+            for row in range(tileYsize):
+                for vTile in range (2):
+                    bitplane0 = 0
+                    bitplane1 = 0
+                    bitplane2 = 0
+                    bitplane3 = 0
+                    for col in range(int(tileXsize/2)):
+                        bitplane = sprite[row][col+(vTile*8)]
+                        bitplane0 = bitplane0 << 1
+                        bitplane0=  bitplane0 | ((bitplane & 1))
+                        bitplane1 = bitplane1 << 1
+                        bitplane1=  bitplane1 | ((bitplane & 2)>>1)
+                        bitplane2 = bitplane2 << 1
+                        bitplane2=  bitplane2 | ((bitplane & 4)>>2)
+                        bitplane3 = bitplane3 << 1
+                        bitplane3=  bitplane3 | ((bitplane & 8)>>3)
+                    destByte = calcDestSpriteByte(row,vTile)
+                    oddBytes[destByte]=bitplane0
+                    oddBytes[destByte+1]=bitplane1
+                    evenBytes[destByte]=bitplane2
+                    evenBytes[destByte+1]=bitplane3
+            oddFile.write(oddBytes)
+            evenFile.write(evenBytes)
+        print ("Created "+oddFileName + " & "+evenFileName+" files")
+
+def calcDestSpriteByte(row,vCol):
+    destByte = 0    
+    if (vCol == 0 ):
+        #We're in the 3rd or 4th Tile of the sprite
+        byteOff= 32
+    else:
+        byteOff = 0
+    row = row * 2 #2 bytes per half row per file
+    destByte = row + byteOff
+    return destByte
+
+
+    
+def convertToFixed(inFile,outFile):
+    print ("Convert to Fixed")
+    img = PIL.Image.open(inFile)
+    img = img.convert('RGB')
+    #Check if image has a MAX of 16 colours
+    RGBColors = img.getcolors()
+    if (len(RGBColors))>16:
+        print ("Image has more than 16 colors, it cannot be converted\Remember that this conversion will use the palette index as index for the color in the resulting file.")
+    colors = [0 for i in range (0,16)]
+    idx=15
+    #For some reason the colors are inverted in the palette....This need to be further checked: TODO
+    for RGBColor in RGBColors:
+        colors[idx]=RGBColor[1]
+        idx = idx -1
+    xsize = img.size[0]
+    ysize = img.size[1]
+    tileXsize = 8
+    tilesPerRow = int(xsize / tileXsize)
+    tileYsize = 8
+    tilesPerCol = int(ysize/ tileYsize)
+    tilesNum = int(tilesPerRow*tilesPerCol)
+    byteFile = bytearray()
+    byteIdx = [16,16,24,24,0,0,8,8]
+    for tile in range (tilesNum):
+        byteOutput = bytearray(32)
+        for col in range(0,tileXsize,2):
+            for row in range(tileYsize):
+                colOff = int(tile%tilesPerRow)
+                rowOff = int(tile/tilesPerRow)
+                pxOff = colOff*tileXsize
+                pyOff = rowOff*tileYsize
+                px = pxOff+col
+                py = pyOff+row
+                pixelL = img.getpixel((px,py))
+                px = pxOff+col+1
+                pixelU = img.getpixel((px,py))
+                pixelLIdx = colors.index (pixelL)
+                pixelUIdx = colors.index (pixelU)
+                byte = (pixelUIdx << 4) | pixelLIdx
+                bytePos = byteIdx[col]+row
+                byteOutput[bytePos]=byte
+        byteFile = byteFile + byteOutput
+    f = open(outFile,'wb')
+    f.write(byteFile)
+    f.close
+    print ("Created "+outFile+" file")
+    
 def convertSprites(inFile,outFile,xsize):
-    print ("I shall convert your sprites into an image!")
+    print ("Converting sprites to image")
     ### Find all sprite files
     # First find where the sequence number is located
     pe = re.compile ("[_-][cC]")
@@ -75,7 +235,6 @@ def convertSprites(inFile,outFile,xsize):
         evenBytes = evenFile.read()
         for sprites in range(0,int(fileSize/64)):
             tile = [[0 for i in range(tileYsize)] for i in range (tileXsize)]
-            print (sprites,end="\r")
             for tileNum in range(4):
                 for row in range(int(tileYsize/2)):
                     oddByte0 = oddBytes[byteNum]
@@ -113,6 +272,7 @@ def convertSprites(inFile,outFile,xsize):
         outFileNum = outFileNum + 1
 
 def convertFixed(inFile,outFile,xsize):
+    print ("Converting fixe tiles to image")
     readFile = open(inFile,'rb')
     statinfo = os.stat(inFile)
     fileSize = statinfo.st_size
@@ -145,7 +305,6 @@ def WriteImage(imgName,xsize,ysize,tileXsize,tileYsize,tiles):
     img = PIL.Image.new('RGB',(xsize, ysize))
     pixels = img.load()
     tilesPerRow = int(xsize/tileXsize)
-    print (len(tiles))
     for pixel in range(0,int(xsize*ysize)):
         imageRow = int(pixel/xsize)
         imageCol = int(pixel%xsize)
@@ -168,7 +327,6 @@ def WriteImage(imgName,xsize,ysize,tileXsize,tileYsize,tiles):
 
 def AltWriteImage(imgName,xsize,ysize,tileXsize,tileYsize,tiles):
     #Save image
-    print (len(tiles))
     img = PIL.Image.new('RGB',(xsize, ysize))
     pixels = img.load()
     tilesPerRow = int(xsize/tileXsize)
@@ -193,6 +351,6 @@ def AltWriteImage(imgName,xsize,ysize,tileXsize,tileYsize,tiles):
             py=py+1
         tRow=tRow+1
     img.save(imgName)
-
+    print ("created ",imgName," file.")
 if __name__ == '__main__':
     main()
